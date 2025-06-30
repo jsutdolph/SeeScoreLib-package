b2cdd3934b5654b603bdca9cd764c876984ba7f0
James Sutton <jrs@jmsutton.co.uk>
2025-06-30 19:07:31 +0100

    Update Package.swift
    store framework in iCloud

--- Package.swift
+++ Package.swift
@@ -18,6 +18,6 @@ let package = Package(
 		// Dependencies declare other packages that this package depends on.
 	],
 	targets: [
-		.binaryTarget(name: "SeeScoreLib", url: "https://www.dropbox.com/scl/fi/fa6fkqxbi9hs2vzuogyf7/SeeScoreLib.xcframework.zip?rlkey=iw9rwg4p7p3ns0us34a0et3sn&st=57p3jx99&dl=0", checksum: "213dbfdbd44a43ee86a19c853ef6958ff1abf87a6b6ad9bda2d683a4b6439628")
+		.binaryTarget(name: "SeeScoreLib", url: "https://www.icloud.com/iclouddrive/0e3khOMpsCqdfweHcLZpgnATw#SeeScoreLib", checksum: "213dbfdbd44a43ee86a19c853ef6958ff1abf87a6b6ad9bda2d683a4b6439628")
 	]
 )
